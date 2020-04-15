b5859ece99a84a6a772848ea3482bd536d3e7b35
Takahisa Sakuma <tsheadbang@gamil.com>
2020-04-15 17:56:10 +0900

    add vuetify

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,17 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <v-app>
+    <v-content class="grey lighten-3">
+      <nuxt />
+    </v-content>
+  </v-app>
 </template>
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+    sans-serif;
   font-size: 16px;
   word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
 }
 
 *,
@@ -22,34 +19,6 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  padding: 0;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,38 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        portfolio-nuxt
-      </h1>
-      <h2 class="subtitle">
-        My priceless Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <v-container class="home" fluid>
+    <div class="home__wrapper text-center">
+      <h1 class="display-3 font-weight-light mb-4">Takahisa Sakuma's Portfolio</h1>
+      <p class="subtitle-1">vue cli&vuetifyの学習を兼ねて作成したポートフォリオサイトです。</p>
     </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  // data() {
+  //   return {
+  //     title: 'Hello, Nuxt!'
+  //   }
+  // },
+  head() {
+    return {
+      title: "Takahisa Sakuma's Portfolio",
+      meta: [{ hid: 'description', name: 'description', content: 'description' }]
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style lang="scss">
+.home {
+  background: linear-gradient(-135deg, #00bcd4, #e0f7fa);
+  width: 100vw;
+  height: 100vh;
+  &__wrapper {
+    width: 60%;
+    max-width: 800px;
+    margin: 0 auto;
+    margin-top: 200px;
+  }
 }
 </style>
